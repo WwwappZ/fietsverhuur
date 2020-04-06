e0fb78fdfedf2a6470984b9f50e9fcb9203f811e
Wouter van Reeven <info@wwwappz.nl>
2020-04-06 23:12:52 +0200

    build client

---
+++ client/build/precache-manifest.6cb789fff1c5cc81b9902a63d13e2b54.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ba1e25185aea6784d42a2d32fd1c777",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4f147260ae9fb2c0d90",
+    "url": "/static/css/2.03407977.chunk.css"
+  },
+  {
+    "revision": "213232b68d02c86b0a28",
+    "url": "/static/css/main.838e941e.chunk.css"
+  },
+  {
+    "revision": "b4f147260ae9fb2c0d90",
+    "url": "/static/js/2.cd5124fd.chunk.js"
+  },
+  {
+    "revision": "dd4c78d87b0b0fb385367f63c60bae5d",
+    "url": "/static/js/2.cd5124fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "213232b68d02c86b0a28",
+    "url": "/static/js/main.f9e9138e.chunk.js"
+  },
+  {
+    "revision": "65762bcbee126357b070",
+    "url": "/static/js/runtime-main.35eb2b37.js"
+  },
+  {
+    "revision": "728ffe8fe9f1210541dc9c2471961a6f",
+    "url": "/static/media/background_login.728ffe8f.jpg"
+  },
+  {
+    "revision": "182cda17e8bf96f7b22c7480cab623b8",
+    "url": "/static/media/background_login_recreatie.182cda17.jpg"
+  },
+  {
+    "revision": "7799dece2c79854f63f09e7dfa528b88",
+    "url": "/static/media/boxed-bg.7799dece.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "cd1696ad384093c532c364b0fd52e9bf",
+    "url": "/static/media/footer.cd1696ad.png"
+  },
+  {
+    "revision": "a770b6797b68e3f8920e473eb824bac0",
+    "url": "/static/media/loader-big.a770b679.gif"
+  },
+  {
+    "revision": "12f0820c451bdc75f4d1ef97732bf6e8",
+    "url": "/static/media/rw-widgets.12f0820c.woff"
+  },
+  {
+    "revision": "792dcd18baf5f544aabcad1883d673c2",
+    "url": "/static/media/rw-widgets.792dcd18.svg"
+  },
+  {
+    "revision": "bc7c4a59f924cf037aad6e1f9edba366",
+    "url": "/static/media/rw-widgets.bc7c4a59.eot"
+  },
+  {
+    "revision": "eceddf474df95d8d4a7e316668c3be85",
+    "url": "/static/media/rw-widgets.eceddf47.ttf"
+  }
+]);
